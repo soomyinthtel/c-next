f67fb640d61ed5c8cd2e7e7184f4f3443462b923
soomyinthtel <gretaoto44@gmail.com>
2025-05-03 00:57:14 +0630

    feat: add types

---
+++ src/types/index.ts
@@ -0,0 +1,33 @@
+export interface Player {
+  id: number;
+  first_name: string;
+  last_name: string;
+  position: string;
+  height: string;
+  weight: string;
+  jersey_number: string | null;
+  college: string | null;
+  country: string;
+  draft_year: number | null;
+  draft_round: number | null;
+  draft_number: number | null;
+  team: PlayerTeam | null;
+}
+export interface PlayerTeam {
+  id: number;
+  conference: string;
+  division: string;
+  city: string;
+  name: string;
+  full_name: string;
+  abbreviation: string;
+}
+
+export interface Team {
+  id: string;
+  name: string;
+  playerCount: number;
+  region: string;
+  country: string;
+  players: Player[];
+}
