e339bc2dbff8e09bc363f93971abf9da605fc05a
soomyinthtel <gretaoto44@gmail.com>
2025-05-03 00:56:26 +0630

    feat: add redux store and hook

---
+++ src/store/index.ts
@@ -0,0 +1,33 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import authReducer from "./authSlice";
+import teamReducer from "./teamSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["teams"],
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  teams: teamReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST"],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
